9391834c62edefb0a90d91cdb019ef92f77f03b4
Anjalie Field <anjaliefield@Anjalie-Fields-MacBook.local>
2017-07-02 17:33:32 -0400

    switch to mysql try 2

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 JOAD tracker, written in mithril
+
+
+REQUIREMENTS:
+npm (Node.js)
+
+PYTHON DEPENDENCES:
+flask
+json
+
+RUN INSTRUCTIONS
+- run "npm install" to install javascript dependencies
+- run "npm start"
+- run python flask.py
+
+START SQL:
+From "Preferences" Panel, start mysql server
+
+INSTALL mysql:
+- install mysql from site using package installer
+- "sudo pip install --egg mysql-connector-python-rf"
--- flaskr.py
+++ flaskr.py
@@ -1,5 +1,6 @@
 import os
-import sqlite3
+#import sqlite3
+import mysql.connector
 from flask import Flask, request, session, g, redirect, url_for, abort, \
      render_template, flash, jsonify
 import json
@@ -11,7 +12,9 @@ app.config.from_object(__name__) # load config from this file , flaskr.py
 
 # Load default config and override config from an environment variable
 app.config.update(dict(
-    DATABASE=os.path.join(app.root_path, 'ga_mithril.db'),
+    DATABASE='ga_mithril_test',
+    DB_USER='ga_joad_RW',
+    DB_PWD='test',
     SECRET_KEY='development key',
     USERNAME='admin',
     DEBUG=True,
@@ -31,17 +34,20 @@ def dict_factory(cursor, row):
 
 def connect_db():
     """Connects to the specific database."""
-    rv = sqlite3.connect(app.config['DATABASE'])
-    rv.row_factory = dict_factory
-    return rv
+#    rv = sqlite3.connect(app.config['DATABASE'])
+    cnx = mysql.connector.connect(user=app.config['DB_USER'],
+                                  password=app.config['DB_PWD'],
+                                  database=app.config['DATABASE'])
+#    rv.row_factory = dict_factory
+    return cnx
 
 def get_db():
     """Opens a new database connection if there is none yet for the
     current application context.
     """
-    if not hasattr(g, 'sqlite_db'):
-        g.sqlite_db = connect_db()
-    return g.sqlite_db
+    if not hasattr(g, 'sql_db'):
+        g.sql_db = connect_db()
+    return g.sql_db
 
 @app.teardown_appcontext
 def close_db(error):
@@ -54,34 +60,43 @@ def load_members_from_db():
     global archer_list
 
     db = get_db()
-    cur = db.execute("""select id,
-                        firstname,
-                        lastname,
-                        byear,
-                        gender
-                        from members order by firstname""")
-    archer_list = cur.fetchall()
+    cur = db.cursor(dictionary=True)
+
+    cur.execute("""select id,
+                   firstname,
+                   lastname,
+                    byear,
+                    gender
+                    from members order by firstname""")
+    archer_list = []
     id_to_archer_details = {}
-    for archer in archer_list:
-         id_to_archer_details[archer["id"]] = archer
+    for row in cur:
+        archer_list.append(row)
+        id_to_archer_details[row["id"]] = row
 
 def load_member_details_from_db():
     global id_to_archer_details
+    global archer_list
 
     db = get_db()
+    cur = db.cursor(dictionary=True)
     # grab the most recent entry for each archer
-    cur = db.execute("""select id,
-                        MAX(date),
-                        discipline,
-                        owns_equipment,
-                        draw_weight,
-                        draw_length,
-                        equipment_description,
-                        distance,
-                        joad_day
-                        from member_details group by id""")
-    row_details = cur.fetchall()
-    for row in row_details:
+    cur.execute("""SELECT a.id,
+                          a.date,
+                          a.discipline,
+                          a.owns_equipment,
+                          a.draw_weight,
+                          a.draw_length,
+                          a.equipment_description,
+                          a.distance,
+                          a.joad_day
+                        FROM member_details a
+                    INNER JOIN
+                        (SELECT id, MAX(date) as max_date
+                         FROM member_details group by id) b
+                    ON a.id = b.id AND a.date = b.max_date
+                    """)
+    for row in cur:
         archer_detail = id_to_archer_details.get(row["id"], None)
         assert archer_detail is not None
         archer_detail.update(row)
@@ -117,8 +132,9 @@ def edit_archer():
                                                equipment_description,
                                                distance,
                                                joad_day)
-                   values (?, ?, ?, ?, ?, ?, ?, ?)"""
+                   values (%s, %s, %s, %s, %s, %s, %s, %s)"""
         db = get_db()
+        cur = db.cursor()
         if "owns_equipment" in data:
             owns_equipment = (1 if data["owns_equipment"] else 0)
         else:
@@ -129,7 +145,7 @@ def edit_archer():
             else:
                 return ""
         assert "id" in data
-        db.execute(query, (get_data("id"),
+        cur.execute(query, (get_data("id"),
                            get_data("discipline"),
                            owns_equipment,
                            get_data("draw_weight"),
@@ -139,6 +155,7 @@ def edit_archer():
                            get_data("joad_day")
                            ))
         db.commit()
+        load_member_details_from_db()
         return ""
 
 @app.route('/add_archer', methods=['POST'])
@@ -146,9 +163,10 @@ def add_archer():
     data = json.loads(request.data)
 
     query = """insert into members (firstname, lastname, gender, byear)
-            values (?, ?, ?, ?)"""
+            values (%s, %s, %s, %s)"""
     db = get_db()
-    db.execute(query, (data['firstname'],
+    cur = db.cursor()
+    cur.execute(query, (data['firstname'],
                        data['lastname'],
                        data['gender'],
                        data['byear']))
@@ -157,33 +175,37 @@ def add_archer():
     load_members_from_db()
     return ""
 
-def get_reschedules(date_str):
+def get_reschedules(date_obj):
     absent_ids = []
     present_ids = []
     db = get_db()
+    cur = db.cursor(dictionary=True)
     query = """select id, from_date, to_date from reschedules where
-              from_date=? OR to_date=?"""
-    cur = db.execute(query, (date_str, date_str))
-    rows = cur.fetchall()
-    for row in rows:
+              from_date=%s OR to_date=%s"""
+    cur.execute(query, (date_obj.isoformat(), date_obj.isoformat()))
+    for row in cur:
         id = row["id"]
-        if row["from_date"] == date_str:
+        if row["from_date"] == date_obj:
             absent_ids.append(id)
-        elif row["to_date"] == date_str:
+        elif row["to_date"] == date_obj:
             present_ids.append(id)
         else:
             assert False
     return absent_ids, present_ids
 
+def sql_format_date(date_str):
+    date_obj = datetime.datetime.strptime(date_str, "%m/%d/%Y").date()
+    return date_obj.isoformat()
+
 def get_attendance_from_db(date_str):
         # first try to pull from attendence table
         db = get_db()
+        cur = db.cursor(dictionary=True)
         query = """select id from attendance where date='""" + date_str + "'"
-        cur = db.execute(query)
-        rows = cur.fetchall()
+        cur.execute(query)
         expected_archers = []
-        if (len(rows) != 0):
-            for archer_id in rows:
+        if (cur.rowcount != 0):
+            for archer_id in cur:
                 id = archer_id["id"]
                 archer = id_to_archer_details.get(id, None)
                 assert archer is not None
@@ -195,6 +217,7 @@ def attendance_list():
     if request.method == "GET":
         date_str = request.args.get('date', None)
         assert date_str is not None
+        date_str = sql_format_date(date_str)
 
         expected_archers = get_attendance_from_db(date_str)
         if expected_archers != []:
@@ -202,10 +225,10 @@ def attendance_list():
                             "set_checked" : True,
                             "message" : "NOTE: Attendance was already entered for this day. Any changes will overwrite" })
 
-        date_obj = datetime.datetime.strptime(date_str, "%m/%d/%Y").date()
+        date_obj = datetime.datetime.strptime(date_str, "%Y-%m-%d").date()
         day_of_week = date_obj.strftime("%A")
 
-        absent_ids, present_ids = get_reschedules(date_str)
+        absent_ids, present_ids = get_reschedules(date_obj)
         expected_archers = []
         for archer_id in id_to_archer_details:
             if archer_id in absent_ids:
@@ -219,6 +242,8 @@ def attendance_list():
         assert request.method == "POST"
         data = json.loads(request.data)
         selected_date = data["date"]
+        formatted_date = sql_format_date(selected_date)
+
         assert selected_date != "", "Error: missing selected date"
         present_list = []  # list of id numbers
         for row in data["rows"]:
@@ -227,16 +252,17 @@ def attendance_list():
 
         # first clear out previous entry
         db = get_db()
-        delete_query = "delete from attendance where date='" + selected_date + "'"
-        db.execute(delete_query)
+        cur = db.cursor()
+        delete_query = "delete from attendance where date='" + formatted_date + "'"
+        cur.execute(delete_query)
         if present_list == []:
             db.commit()
             return jsonify({"message" : "Marked no archers as present"})
         query = "insert into attendance (date, id) values "
         for id in present_list:
-            query += "('" + selected_date + "'," + str(id) + "), "
+            query += "('" + formatted_date + "'," + str(id) + "), "
         # cut off trailing comma space
-        db.execute(query[:-2])
+        cur.execute(query[:-2])
         db.commit()
         return jsonify({"message" : "Attendance table updated"})
 
@@ -244,9 +270,9 @@ def attendance_list():
 def get_date_or_null(date_str):
     try:
         date = datetime.datetime.strptime(date_str, "%m/%d/%Y")
-        return date_str
+        return date.isoformat()
     except:
-        return "NULL"
+        return None
 
 @app.route('/reschedule', methods=['POST'])
 def reschedule():
@@ -260,17 +286,18 @@ def reschedule():
     to_date_sql = get_date_or_null(to_date_str)
 
      # TODO: add error checking on client side
-    assert to_date_sql != "NULL" or from_date_sql != "NULL"
+    assert to_date_sql is not None or from_date_sql is not None
 
     db = get_db()
+    cur = db.cursor()
     # need to clear out any old reschedules
-    if (not from_date_sql == "NULL"):
-        delete_query = """DELETE FROM reschedules where id=? and to_date=?"""
-        db.execute(delete_query, (id, from_date_sql))
+    if (not from_date_sql is None):
+        delete_query = """DELETE FROM reschedules where id=%s and to_date=%s"""
+        cur.execute(delete_query, (id, from_date_sql))
 
     insert_query = """INSERT INTO reschedules (id, from_date, to_date, note)
-                            VALUES (?, ?, ?, ?)"""
-    db.execute(insert_query, (id, from_date_sql, to_date_sql, "\'\'"))
+                            VALUES (%s, %s, %s, %s)"""
+    cur.execute(insert_query, (id, from_date_sql, to_date_sql, "\'\'"))
     db.commit()
     return jsonify({})
 
@@ -282,12 +309,13 @@ def extra_practice():
     date_str = data["date"]
     date_sql = get_date_or_null(date_str)
     # TODO: client side checking
-    if date_sql == "NULL":
+    if date_sql is None:
         return jsonify({})
     else:
         db = get_db()
-        query = "INSERT INTO attendance (date, id, is_joad_practice) VALUES (?, ?, ?)"
-        db.execute(query, (date_sql, id, 0))
+        cur = db.cursor()
+        query = "INSERT INTO attendance (date, id, is_joad_practice) VALUES (%s, %s, %s)"
+        cur.execute(query, (date_sql, id, 0))
         db.commit()
     return jsonify({})
 
@@ -306,14 +334,14 @@ def create_new_form_list(old_form_list):
 def get_form_notes_by_attendance(date_str):
         # first try to pull from attendence table
         db = get_db()
+        cur = db.cursor(dictionary=True)
         query = """select id from attendance where date='""" + date_str + "'"
-        cur = db.execute(query)
-        rows = cur.fetchall()
+        cur.execute(query)
         expected_archers = {}
-        if (len(rows) == 0):
+        if (cur.rowcount == 0):
             return expected_archers
         else:
-            for archer_id in rows:
+            for archer_id in cur:
                 id = archer_id["id"]
                 archer = id_to_archer_details.get(id, None)
                 assert archer is not None
@@ -321,15 +349,15 @@ def get_form_notes_by_attendance(date_str):
 
             # pull form for all from db, then filter by attendance
             form_query = """SELECT a.id, a.date, a.category, a.status, a.note, a.instructor FROM form_notes a
-                            JOIN (SELECT MAX(date) as max_date, id, category, status, note, instructor
-                                  FROM form_notes f GROUP BY f.id)f
+                            JOIN (SELECT MAX(date) as max_date, id
+                                  FROM form_notes GROUP BY id) f
                             ON (a.date = max_date and a.id = f.id);"""
-            cur = db.execute(form_query)
-            rows = cur.fetchall()
-            for row in rows:
+            cur.execute(form_query)
+            for row in cur:
                 archer_data = expected_archers.get(row["id"], None)
                 if archer_data is None:
                     continue
+                row["date"] = row["date"].strftime("%m/%d/%Y")  # for better display
                 form_list = archer_data.get("form_list", [])
                 form_list.append(row)
                 archer_data["form_list"] = form_list
@@ -337,9 +365,8 @@ def get_form_notes_by_attendance(date_str):
             # first check if we've already entered for this date
             new_form_query = """SELECT id, date, category, status, note, instructor FROM form_notes
                                 WHERE date='""" + date_str + """'"""
-            cur = db.execute(new_form_query)
-            rows = cur.fetchall()
-            for row in rows:
+            cur.execute(new_form_query)
+            for row in cur:
                 archer_data = expected_archers.get(row["id"], None)
                 if archer_data is None:
                     continue
@@ -377,6 +404,7 @@ def form_notes():
     if request.method == "GET":
         date_str = request.args.get('date', None)
         assert date_str is not None
+        date_str = sql_format_date(date_str)
 
         expected_archers = get_form_notes_by_attendance(date_str)
         if expected_archers != {}:
@@ -390,11 +418,12 @@ def form_notes():
     else:
         data = json.loads(request.data)
 
-        selected_date = data["date"]
+        selected_date = sql_format_date(data["date"])
         id_to_form_list = data["id_to_form_list"]
         db = get_db()
+        cur = db.cursor()
         delete_query = "delete from form_notes where date='" + selected_date + "'"
-        db.execute(delete_query)
+        cur.execute(delete_query)
 
         query = """insert into form_notes
                    (id, date, category, status, note, instructor) values """
@@ -410,7 +439,7 @@ def form_notes():
                           + get_data_or_null("note", data) + ", "
                           + get_data_or_null("instructor", data) + "), ")
         # cut off trailing comma space
-        db.execute(query[:-2])
+        cur.execute(query[:-2])
         db.commit()
         return jsonify({"message" : "Form table updated"})
 
@@ -419,20 +448,25 @@ def score_entry():
     if request.method == "GET":
         date_str = request.args.get('date', None)
         assert date_str is not None
+        date_str = sql_format_date(date_str)
         query = """select
                    date, id, distance, target_size, is_tournament, number_rounds,
                    arrows_per_round, score, total_score, note from scores
                    WHERE date='""" + date_str + """'"""
         db = get_db()
-        cur = db.execute(query)
-        rows = cur.fetchall()
-        for row in rows:
-            row['score'] = row['score'].split(';')
+        cur = db.cursor(dictionary=True)
+        cur.execute(query)
+        rows = []
+        for row in cur:
+            new_row = {}
+            new_row.update(row)
+            new_row['score'] = new_row['score'].split(';')
             archer_detail = id_to_archer_details.get(row["id"], None)
             assert archer_detail is not None
             # TODO: cleaner way to do this?
             for key in archer_detail:
-                row[key] = archer_detail[key]
+                new_row[key] = archer_detail[key]
+            rows.append(new_row);
         message = ""
         if (len(rows) != 0):
             message = "NOTE: Scores previously entered for this date. Changes will overwrite"
@@ -441,7 +475,7 @@ def score_entry():
     else:
         data = json.loads(request.data)
 
-        selected_date = data["date"]
+        selected_date = sql_format_date(data["date"])
         rows = data["rows"]
 
         query = """insert into scores
@@ -463,11 +497,12 @@ def score_entry():
                       str(row['total_score']) + ", " +
                       get_data_or_null("note", row) + "), ")
         db = get_db()
+        cur = db.cursor()
         delete_query = "delete from scores where date='" + selected_date + "'"
-        db.execute(delete_query)
+        cur.execute(delete_query)
 
         # cut off trailing comma space
-        db.execute(query[:-2])
+        cur.execute(query[:-2])
         db.commit()
 
         return jsonify({"message" : "Score table updated"})
