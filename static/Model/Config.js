b7d89a5f38539f467bc4f66d87fcae3166b189d8
Anjalie Field <anjaliefield@Anjalie-Fields-MacBook.local>
2017-07-30 16:14:10 -0400

    Add authorizers to serverless cfg file

--- static/Model/Config.js
+++ static/Model/Config.js
@@ -3,7 +3,7 @@ module.exports = {
   USER_POOL_ID : 'us-east-1_kNYzIyhAX',
   CLIENT_ID : '5hirip2bfagqf5pq0gflihm13b',
   REGION : 'us-east-1',
-  BASE_URL : 'https://936m4264y0.execute-api.us-east-1.amazonaws.com/dev/ga_joad'
+  BASE_URL : 'https://hu5jvdm5og.execute-api.us-east-1.amazonaws.com/dev/ga_joad'
 }
 
 
